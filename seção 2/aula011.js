74695a72e322e198aa880c5a593c072a11481122
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-01-22 21:29:17 -0300

    att

---
+++ seção 2/aula011.js
@@ -0,0 +1,11 @@
+// Comentário
+console.log('Olá Mundo!'); // Outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+/*
+COMENTÁRIO
+GRANDE
+*/
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
