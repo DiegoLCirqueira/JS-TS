3463a6c7e59c02d3eee2ac135a9dc95bc47181a6
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-01-29 19:26:15 -0300

    att e EXERCÍCIO

--- seção 2/aula019.js
+++ seção 2/aula019.js
@@ -1,6 +1,6 @@
 const nome = 'João'; //string
 const nome1 = "João"; //string
-const nome2 = João; //string
+const nome2 = "João"; //string
 const num1 = 10; //number
 const num2 = 10.52; //number
 let nomeAluno; //undefined -> não aponta pra local nenhum na memória
