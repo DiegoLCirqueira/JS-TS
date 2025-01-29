0b6cd9d54f644bbbc27bb4d10b0e037e2840b2ff
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-01-29 18:26:44 -0300

    att

---
+++ seção 2/aula021/JS/script.js
@@ -0,0 +1,4 @@
+const num1 = parseFloat(prompt('Digite um número'))
+const num2 = parseFloat(prompt('Digite outro número'))
+const resultado = ( num1 + num2)
+alert(`O resultaod desta soma é: ${resultado}`)
