2e15d0cfeab6dcf4cefa06dad8275017287f15ad
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-02-05 17:04:17 -0300

    Math

---
+++ seção 2/aula027.js
@@ -0,0 +1,18 @@
+let num1 = 9.55;
+//console.log(num1 ** .5) - vai mostrar a raiz de tal número;
+
+//let num2 = Math.floor(num1); - Vai arredondar para baixo;
+
+//let num2 = Math.ceil(num1); - Vai arredondar para cima;
+
+//let num2 = Math.round(num1); - Vai arredondar para o número mais próximo;
+
+//console.log(Math.max(1,2,3,4,5,6,7,8,9,10)); - Vai mostrar o maior número da sequência;
+
+//console.log(Math.min(1,2,3,4,5,6,7,8,9,10)); - Vai mostrar o menor número da sequência;
+
+//const aleatorio = Math.random() * (10 - 5) + 5; - Vai gerar números aleatórios entre 10 e 5 quebrados;
+
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 5); - Vai gerar números aleatórios entre 10 e 5 inteiros;
+
+console.log(aleatorio);
