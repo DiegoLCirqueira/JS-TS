f226f5cdfed0afd9eb795e270d6d9b666ffe8548
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-02-05 16:46:47 -0300

    numeros

---
+++ seção 2/aula026.js
@@ -0,0 +1,18 @@
+let num1  = 14.5;
+let num2 = 2.5;
+
+num1 += num2;
+//num1 = Number(num1.toFixed(2)); - Transforma o número em inteiro realmente;
+
+console.log(num1);
+
+
+//console.log(num1.toString() + num2);
+//num1 = num1.toString(); - Trânsforma em string;
+//console.log(num1.toString(2)); - Mostra o código binário do valor;
+//console.log(num1.toFixed(2)); - Vai definir quantos números vão aparecer dps dp ponto;
+//console.log(Number.isInteger(num1)); - Vai mostrar com True ou False se o número é inteiro ou não;
+
+//let temp = num1 * 'Olá';
+//console.log(Number.isNaN(temp)); - Vai mostrar se a variável é ou não um NaN;
+//console.log(temp);
