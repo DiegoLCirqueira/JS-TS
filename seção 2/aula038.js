e3cdeaf3ff67a0e31347f00960c5b97b07341e59
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-02-18 16:00:24 -0300

    operadores lógicos

---
+++ seção 2/aula038.js
@@ -0,0 +1,8 @@
+/*
+Operadores Lógicos
+&& -> AND - Precisa que todas sejam verdadeiras para retornar True;
+|| -> OR - Precisa de pelo menos uma expressão True para dar esse resultado, caso n tenha será False;
+! -> NOT - Vai inverter o valor de True para False ou ao contrário;
+*/
+const and = true && true;
+const or = true || false;
