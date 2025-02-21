beaced50270abe9bc747f9f91171b8859ee7fb48
Diego Lins Cirqueira <diegolinscirqueira@hotmail.com>
2025-02-21 00:05:38 -0300

    uso da ? e :

---
+++ seção 2/aula045.js
@@ -0,0 +1,11 @@
+// condição ? 'Valor para verdadeiro' : 'Valor para falso';
+const ponturacaoUsuario = 1000;
+const nivelUsuario = ponturacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+
+console.log(nivelUsuario)
+
+// if (ponturacaoUsuario >= 1000){
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuario normal');
+// }
